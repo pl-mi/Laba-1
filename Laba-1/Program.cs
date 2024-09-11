b9276d1b150eeaccee2399c65d71257c6e2ca998
pl-mi <mihailpluha@gmail.com>
2024-09-11 16:00:34 +0300

    Add project files.

---
+++ Laba-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s1 = "бiологiя алгебра iсторiя географiя геометрiя"; string[] a;
+        a = s1.Split(' '); Array.Reverse(a);
+        string s2 = string.Join(" ", a); Console.WriteLine(s2);
+        Console.ReadKey();
+    }
+}
